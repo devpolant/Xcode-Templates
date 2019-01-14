187e6a42e1cfc58fe58096c03ccb4c4bc5c18ea6
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2019-01-14 12:24:49 +0200

    Fixed SQLTable+GRDB plist.

--- Collections/CellViewModel+TableView.xctemplate/___FILEBASENAME___TableViewCell.swift
+++ Collections/CellViewModel+TableView.xctemplate/___FILEBASENAME___TableViewCell.swift
@@ -9,7 +9,7 @@ final class ___VARIABLE_modelName___TableViewCell: UITableViewCell {
     
     // MARK: - Init
     
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setup()
     }
