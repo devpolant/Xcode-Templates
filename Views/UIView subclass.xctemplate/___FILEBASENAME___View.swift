0d7afe6f42ddf611c6b20e92f458e93cb5ce2042
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2019-02-25 15:45:40 +0200

    Added template for UIView subclass

---
+++ Views/UIView subclass.xctemplate/___FILEBASENAME___View.swift
@@ -0,0 +1,29 @@
+//___FILEHEADER___
+
+import UIKit
+import SnapKit
+
+final class ___VARIABLE_viewName___View: UIView {
+    
+    // MARK: - Views
+    
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    
+    // MARK: - Setup
+    
+    private func setup() {
+        
+    }
+}
